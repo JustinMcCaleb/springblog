b79b51ffde4e37f32f1f89d6c4f258efa702c8f4
Justin McCaleb <justinleemccaleb@gmail.com>
2020-07-31 16:20:57 -0500

    view lecture files

---
+++ src/main/java/com/codeup/springblog/ProfileController.java
@@ -0,0 +1,25 @@
+package com.codeup.springblog;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class ProfileController {
+
+    @GetMapping("/profile/{username}")
+    public String profile(@PathVariable String username, Model model){
+        model.addAttribute("user", username);
+        return "profile";
+    }
+
+    @PostMapping("/profile")
+    public String viewLoginInfo(@RequestParam(name = "user") String username, @RequestParam(name = "password") String password, Model model){
+        model.addAttribute("username", username);
+        model.addAttribute("password", password);
+        return "profile";
+    }
+}
