3f1ef0cba9723e26a27a03b38ff79bed26d78f25
Justin McCaleb <justinleemccaleb@gmail.com>
2020-08-01 11:49:01 -0500

    completed dice roll exercise

--- src/main/java/com/codeup/springblog/DiceController.java
+++ src/main/java/com/codeup/springblog/DiceController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.Random;
+
 @Controller
 public class DiceController {
 
@@ -16,7 +18,16 @@ public class DiceController {
 
     @GetMapping("/roll-dice/{n}")
     public String result(@PathVariable int n, Model model){
+        //random number
+        Random rand = new Random();
+        int num = rand.nextInt(6) + 1;
+        boolean guessedRight = false;
+        if (n == num){
+            guessedRight = true;
+        }
+        model.addAttribute("guessedRight", guessedRight);
+        model.addAttribute("diceRoll", num);
         model.addAttribute("userGuess", n);
-        return "dice";
+        return "dice-result";
     }
 }
