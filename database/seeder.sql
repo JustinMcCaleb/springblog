0a97372c725c7dee9e261525f01e3021d80c2f1b
Justin McCaleb <justinleemccaleb@gmail.com>
2020-08-04 15:02:18 -0500

    completed JPA exercise

---
+++ database/seeder.sql
@@ -0,0 +1,8 @@
+use blog_db;
+
+INSERT INTO posts (title, body)
+VALUES ('UFO', 'It abducted my lizard. Poor Clementine.'),
+       ('Ghost','It walked through walls man.'),
+       ('Clementine Returns!','Clementine showed back up and stares at me while I sleep now. Not cool.'),
+       ('My Lizard Is Trying To Kill Me','If I stop posting please call the cops. It was 100% Clementine.'),
+       ('I\'m totally fine', 'HAHA, wat a gret joke, I AM FINE and definetly a human not a lizzard. Clementine is my best friend');
