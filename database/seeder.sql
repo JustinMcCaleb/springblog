4f594f74408cfff7b411ad79328519d5fb633622
Justin McCaleb <justinleemccaleb@gmail.com>
2020-08-04 15:30:34 -0500

    updated styling

--- database/seeder.sql
+++ database/seeder.sql
@@ -5,4 +5,9 @@ VALUES ('UFO', 'It abducted my lizard. Poor Clementine.'),
        ('Ghost','It walked through walls man.'),
        ('Clementine Returns!','Clementine showed back up and stares at me while I sleep now. Not cool.'),
        ('My Lizard Is Trying To Kill Me','If I stop posting please call the cops. It was 100% Clementine.'),
-       ('I\'m totally fine', 'HAHA, wat a gret joke, I AM FINE and definetly a human not a lizzard. Clementine is my best friend');
+       ('I\'m totally fine', 'HAHA, wat a gret joke, I AM FINE and definetly a human not a lizzard. Clementine is my best friend'),
+       ('Title1','Body1'),
+       ('Title2','Body2'),
+       ('Title3','Body3'),
+       ('Title4','Body4'),
+       ('Title5','Body5');
