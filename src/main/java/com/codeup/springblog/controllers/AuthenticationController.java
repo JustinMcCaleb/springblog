9622095cfd813210ef1605359566f2ca1355a998
Justin McCaleb <justinleemccaleb@gmail.com>
2020-08-11 11:51:41 -0500

    added spring security files

--- src/main/java/com/codeup/springblog/controllers/AuthenticationController.java
+++ src/main/java/com/codeup/springblog/controllers/AuthenticationController.java
@@ -4,11 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class LoginController {
-
-//    @GetMapping("/login")
-//    public String viewLoginForm(){
-//        return "login";
-//    }
-
+public class AuthenticationController {
+    @GetMapping("/login")
+    public String showLoginForm() {
+        return "users/login";
+    }
 }
